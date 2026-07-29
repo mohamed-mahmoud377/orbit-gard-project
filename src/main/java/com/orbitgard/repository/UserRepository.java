452f6cfaf0852912a91fa19dd2eb011331c863e4
Shapinam <shapinam.abdelfattah@gmail.com>
2026-07-29 16:38:18 +0300

    Activate the account api added

--- src/main/java/com/orbitgard/repository/UserRepository.java
+++ src/main/java/com/orbitgard/repository/UserRepository.java
@@ -3,5 +3,7 @@ package com.orbitgard.repository;
 import com.orbitgard.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
 }
--- src/main/java/com/orbitgard/repository/VerificationTokenRepository.java
+++ src/main/java/com/orbitgard/repository/VerificationTokenRepository.java
@@ -4,8 +4,9 @@ import com.orbitgard.entity.VerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, UUID> {
 
     Optional<VerificationToken> findByTokenHash(String tokenHash);
 }
