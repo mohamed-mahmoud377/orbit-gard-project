06e07799d3e81b6ca7810a5b0095c664639d25a0
farahsharshar <Farah.hassan05@eng-st.cu.edu.eg>
2026-08-11 14:01:32 +0300

    fixed swagger auth problem for session and profile

--- backend/src/main/java/com/orbitgard/controller/ProfileController.java
+++ backend/src/main/java/com/orbitgard/controller/ProfileController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/profile")
+@SecurityRequirement(name = "bearerAuth")
 @Tag(name = "Profile", description = "View and update the authenticated user's profile")
 public class ProfileController {
 
